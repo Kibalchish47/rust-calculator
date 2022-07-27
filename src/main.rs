7cb6af8267692a6b5b3ee8acb72249c74b3a9738
Daniel D <kibalchishthecoder@gmail.com>
2022-07-27 22:03:06 +0300

    gfndj ihgslgh ureidsogwerhbuoih
    Co-authored-by: Sammy99jsp <Sammy99jsp@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::io::{self, Write};
+
+enum Operation {
+    Add,
+    Sub,
+    Mult,
+    Div,
+}
+
+impl Operation {
+    fn evaluate(&self, num1: u32, num2 : u32) -> u32 {
+        match self {
+            Operation::Add  => num1 + num2, // returns num1 + num2 
+            Operation::Sub  => num1 - num2, // returns ... 
+            Operation::Mult => num1 * num2, // returns ...
+            Operation::Div  => num1 / num2, // returns ...
+        }
+    }
+
+    fn from_string(operation: String) -> Result<Self, String> {
+        match operation.trim() {
+            "+" => Ok(Operation::Add),
+            "-" => Ok(Operation::Sub),
+            "*" => Ok(Operation::Mult),
+            "/" => Ok(Operation::Div),
+            &_ => Err("Error. ".to_string()),
+        }
+    }
+}
+
+struct Input;
+
+impl Input {
+    pub fn string(msg : &str) -> String {
+        let mut input:String = String::new();
+        print!("{}", msg);
+    
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+    
+        return input;
+    }
+    
+    fn u32(msg: &str) -> u32 {
+        Self::string(msg).trim().parse().expect("Not a number!")
+    }
+}
+
+pub fn main() {
+    println!("TEXT-BASED CALCULATOR");
+    loop{
+        println!("");
+        let num1 = Input::u32("Enter the first number : ");
+        let num2 = Input::u32("Enter the second number : ");
+
+        let operation = Input::string("Choose your operation (+ - * /) : ");
+
+        let operation = Operation::from_string(operation.to_string()).unwrap();
+
+        let result : u32 =  operation.evaluate(num1, num2);
+
+        println!("");
+        println!("The result of the calculation is : {}", result);
+    }
+}
