185a3aa28c1c293536f09f325200b2ae7968b224
Daniel D <kibalchishthecoder@gmail.com>
2022-08-02 21:11:26 +0300

    Co-authored-by: Sammy99jsp <Sammy99jsp@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 use std::io::{self, Write};
 
-enum Operation {
+enum Operation { // every member is an operation : + - * / 
     Add,
     Sub,
     Mult,
     Div,
+    Exp,
 }
 
 impl Operation {
     fn evaluate(&self, num1: u32, num2 : u32) -> u32 { // this function defines what is the operation chosen by the user
         match self {
-            Operation::Add  => num1 + num2, // returns num1 + num2 
-            Operation::Sub  => num1 - num2, // returns ... -
-            Operation::Mult => num1 * num2, // returns ... *
-            Operation::Div  => num1 / num2, // returns ... /
+            Operation::Add  => num1 + num2,    // returns num1 + num2 
+            Operation::Sub  => num1 - num2,    // returns ... -
+            Operation::Mult => num1 * num2,    // returns ... *
+            Operation::Div  => num1 / num2,    // returns ... /
+            Operation::Exp  => num1.pow(num2), // returns ... **
         }
     }
 
-    fn from_string(operation: String) -> Result<Self, String> {
+    fn from_string(operation: String) -> Result<Self, String> { // associates the inputted operations with the operations in the enum
         match operation.trim() {
-            "+" => Ok(Operation::Add),
-            "-" => Ok(Operation::Sub),
-            "*" => Ok(Operation::Mult),
-            "/" => Ok(Operation::Div),
-            &_ => Err("Error. ".to_string()), // error handling
+            "+"  => Ok(Operation::Add),  // does an addition 
+            "-"  => Ok(Operation::Sub),  // does a substraction
+            "*"  => Ok(Operation::Mult), // does a multiplication 
+            "/"  => Ok(Operation::Div),  // does a division
+            "**" => Ok(Operation::Exp),  // does an exponent
+            &_   => Err("Error. ".to_string()), // error handling
         }
     }
 }
 
 struct Input;
 
-impl Input 
+impl Input // grouping together helper functions 
 {
-    pub fn string(msg : &str) -> String {
-        let mut input:String = String::new();
+    pub fn string(msg : &str) -> String { // takes an input (num1 and num2) and returns it 
+        let mut input : String = String::new();
         print!("{}", msg);
     
         io::stdout().flush().unwrap();
@@ -44,26 +47,36 @@ impl Input
         return input;
     }
     
-    fn u32(msg: &str) -> u32 { // this function checks if the input is a number to begin with
-        Self::string(msg).trim().parse().expect("Not a number!")
+    fn u32(msg: &str) -> Option<u32> { // this function checks if the input is a number to begin with
+        match Self::string(msg).trim() {
+            // Load value from memory 
+            "m" => None,
+            // If it's not "m", normal parsing 
+             s  => Some(s.parse().expect("Not a number!")),
+        }
     }
 }
 
 pub fn main() 
 {
-    println!("TEXT-BASED CALCULATOR");
+    println!("TEXT-BASED CALCULATOR ! ");
+    println!("To use the result of the previous calculation as a value, enter 'm' instead of the number (the value is 0 by default) !");
+
+    let mut memory:u32 = 0;
     loop{
         println!("");
-        let num1 = Input::u32("Enter the first number : ");
-        let num2 = Input::u32("Enter the second number : ");
+        let num1 = Input::u32("Enter the first number : ").unwrap_or(memory);
+        let num2 = Input::u32("Enter the second number : ").unwrap_or(memory);
 
-        let operation = Input::string("Choose your operation (+ - * /) : ");
+        let operation = Input::string("Choose your operation (+ - * / **) : ");
 
         let operation = Operation::from_string(operation.to_string()).unwrap();
 
         let result:u32 =  operation.evaluate(num1, num2);
 
         println!("");
-        println!("The result of the calculation is : {}", result);
+        println!("The result of the calculation is : {} (it is a u32)!", result);
+
+        memory = result; 
     }
 }
